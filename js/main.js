903cd55aa585993f0fc21a2dd4bd25ebc145fcce
Anton Nilsson <antonnilssondeveloper@gmail.com>
2018-05-31 12:46:51 +0200

    Fixed download link for resume

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var $grid = $('.resume-items').isotope('layout');
 
 $('.resume-filter').on( 'click', 'a', function(e) {
-	e.preventDefault();
   	var filterValue = $(this).attr('data-filter');
   	$grid.isotope({ filter: filterValue });
 
